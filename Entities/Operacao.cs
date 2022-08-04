d112df2e68e7d90785579e890849e1d871e0c08c
ArturChiodiSiqueira <arturchiodi@outlook.com>
2022-08-04 11:21:24 -0300

    Add project files.

---
+++ Entities/Operacao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PoupaDev.Enums;
+
+namespace PoupaDev.Entities
+{
+    
+        public class Operacao
+        {
+            public Operacao(decimal valor, TipoOperacao tipo)
+            {
+                Id = new Random().Next(1, 1000);
+                Valor = valor;
+                Tipo = tipo;
+
+                DataOperacao = DateTime.Now;
+            }
+
+            public int Id { get; private set; }
+            public decimal Valor { get; private set; }
+            public TipoOperacao Tipo { get; private set; }
+            public DateTime DataOperacao { get; set; }
+        }
+    
+}
---
+++ Enums/TipoOperacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoupaDev.Enums
+{
+    public enum TipoOperacao
+    {
+       
+            Saque = 0,
+            Deposito = 1
+        
+    }
+}
